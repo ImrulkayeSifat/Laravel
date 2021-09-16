1b4934b5e70a3fcfaea5555809e280b64479d52f
Imrul Kayes Sifat <41978816+ImrulkayeSifat@users.noreply.github.com>
2021-09-16 09:47:30 +0600

    stub customization that create controller

---
+++ app/Http/Controllers/StudentnewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentnewController extends Controller
+{
+    //php artisan stub:publish
+    public function create()
+    {
+
+    }
+}
